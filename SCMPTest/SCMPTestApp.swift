e0fb07ee17bdf37d24ed2ead64bcb653ddd37eb4
Andy Li <andy.li@dimorder.com>
2023-10-23 14:47:24 +0800

    Initial Commit

---
+++ SCMPTest/SCMPTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCMPTestApp.swift
+//  SCMPTest
+//
+//  Created by Andy Li on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SCMPTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
